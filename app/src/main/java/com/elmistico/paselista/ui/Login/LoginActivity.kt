6d6436dac04cf68fb939c8568671a2786257004f
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2025-07-18 22:28:30 -0600

    login y registro aparentemete ya terminado, a lo mejor mañana le pongo mas cosas

--- app/src/main/java/com/elmistico/paselista/ui/Login/LoginActivity.kt
+++ app/src/main/java/com/elmistico/paselista/ui/Login/LoginActivity.kt
@@ -1,6 +1,10 @@
 package com.elmistico.paselista.ui.Login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +20,29 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val googleBtn = findViewById<ImageView>(R.id.imageView)
+        val appleBtn = findViewById<ImageView>(R.id.imageView2)
+        val fbBtn = findViewById<ImageView>(R.id.imageView3)
+        val forgot = findViewById<TextView>(R.id.textView3)
+        val textViewRegistrer = findViewById<TextView>(R.id.textViewRegistrer)
+
+        googleBtn.setOnClickListener{
+            Toast.makeText(this, "Iniciar sesión con Google", Toast.LENGTH_SHORT).show()
+        }
+        appleBtn.setOnClickListener{
+            Toast.makeText(this, "Iniciar sesión con Apple", Toast.LENGTH_SHORT).show()
+        }
+        fbBtn.setOnClickListener{
+            Toast.makeText(this, "Iniciar sesión con Facebook", Toast.LENGTH_SHORT).show()
+        }
+        forgot.setOnClickListener{
+            Toast.makeText(this,"Olvidaste tu contraseña???", Toast.LENGTH_SHORT).show()
+        }
+
+        textViewRegistrer.setOnClickListener{
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
         }
     }
 }
