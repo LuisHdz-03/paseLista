102cf06234b9b9b0b157366ba35327f759325653
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2025-07-27 20:29:06 -0600

    nose

--- app/src/main/java/com/elmistico/paselista/ui/docente/DocenteHomeActivity.kt
+++ app/src/main/java/com/elmistico/paselista/ui/docente/DocenteHomeActivity.kt
@@ -1,4 +1,13 @@
 package com.elmistico.paselista.ui.docente
 
-class DocenteHomeActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.elmistico.paselista.R
+
+class DocenteHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_docente_home)
+    }
 }
