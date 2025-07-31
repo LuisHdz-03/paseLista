4dd162201cdcd111d311fd58e4b1028580f7cf27
Luis Hernandez <hegl030420@itsuruapan.edu.mx>
2025-07-31 15:43:44 -0600

    madretipovienestar

--- app/src/main/java/com/elmistico/paselista/ui/login/LoginActivity.kt
+++ app/src/main/java/com/elmistico/paselista/ui/login/LoginActivity.kt
@@ -3,17 +3,12 @@ package com.elmistico.paselista.ui.login
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.elmistico.paselista.R
 import com.elmistico.paselista.ui.alumno.AlumnoHomeActivity
-import com.elmistico.paselista.ui.docente.DocenteHomeActivity
-import com.elmistico.paselista.ui.registro.RegistroActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,33 +20,10 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val googleBtn = findViewById<ImageView>(R.id.imageView)
-        val appleBtn = findViewById<ImageView>(R.id.imageView2)
-        val fbBtn = findViewById<ImageView>(R.id.imageView3)
-        val forgot = findViewById<TextView>(R.id.textView3)
-        val textViewRegistrer = findViewById<TextView>(R.id.textViewRegistrer)
         val btnLogin = findViewById<Button>(R.id.buttonLogin)
 
-        googleBtn.setOnClickListener{
-            Toast.makeText(this, "Iniciar sesión con Google", Toast.LENGTH_SHORT).show()
-        }
-        appleBtn.setOnClickListener{
-            Toast.makeText(this, "Iniciar sesión con Apple", Toast.LENGTH_SHORT).show()
-        }
-        fbBtn.setOnClickListener{
-            Toast.makeText(this, "Iniciar sesión con Facebook", Toast.LENGTH_SHORT).show()
-        }
-        forgot.setOnClickListener{
-            Toast.makeText(this,"Olvidaste tu contraseña???", Toast.LENGTH_SHORT).show()
-        }
-
-        textViewRegistrer.setOnClickListener{
-            val intent = Intent(this, RegistroActivity::class.java)
-            startActivity(intent)
-        }
-
         btnLogin.setOnClickListener{
-            val intent = Intent(this, DocenteHomeActivity::class.java)
+            val intent = Intent(this, AlumnoHomeActivity::class.java)
             startActivity(intent)
         }
 
